a4da703d5b27d82eaa8b6d1d3dac5b0b70ed4a24
troyleak <troy.leak@gmail.com>
2015-03-18 12:22:37 -0700

    Created
    Project created

---
+++ Spontanuity/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spontanuity::Application.config.secret_key_base = '425055edc684828b58ed427c96c6b71518b3904fbc7d48c143ecbf7d647d661e65f5809dda134c4dfe761fdc5227c12ddae2d64bfa7ec01a76638e1e7eb544d9'
